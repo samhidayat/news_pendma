1be6ad5361bfe174f1993ba8a5011f61e14dd2a6
Sam Hidayat <samhidayat0@gmail.com>
2024-05-24 22:35:18 +0700

    Update koneksi.php

--- informasi/koneksi.php
+++ informasi/koneksi.php
@@ -1,8 +1,8 @@
 <?php
 $host = "localhost"; // Nama hostnya
-$username = "mtsyatab_untuk_coba2"; // Username
-$password = "alya04052007"; // Password (Isi jika menggunakan password)
-$database = "mtsyatab_untuk_coba1"; // Nama databasenya
+$username = "newspendma2"; // Username
+$password = "newspendma654321"; // Password (Isi jika menggunakan password)
+$database = "newspendma2"; // Nama databasenya
 
 $connect = mysqli_connect($host, $username, $password, $database); // Koneksi ke MySQL
 ?>
